47a823775f4f47467088e383f0ce7670b854de2b
Thirumalai Samy Rajkumar <thrmlsam@gmail.com>
2014-04-23 20:15:38 -0400

    Initial commit

---
+++ src/pubsub/publisher/utils/Constants.java
@@ -0,0 +1,15 @@
+package pubsub.publisher.utils;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author thirumalaisamy
+ */
+public class Constants {
+    public static final int BROKER_PORT = 8080;
+}
